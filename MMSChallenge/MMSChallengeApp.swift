fa35f7c92b2bdaeb99095432de16398f05f432ca
Raditya <raditya.learner@gmail.com>
2022-08-24 19:11:56 +0800

    Initial Commit

---
+++ MMSChallenge/MMSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMSChallengeApp.swift
+//  MMSChallenge
+//
+//  Created by CTSS Students on 24/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MMSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
